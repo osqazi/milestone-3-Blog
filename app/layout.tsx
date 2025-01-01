db10f3b7b7b50eda005a86bc1bd21619e7a5aff0
osqazi <osqazi@gmail.com>
2025-01-01 16:59:06 +0500

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Bebas_Neue } from "next/font/google";
 import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
 import Footer from "./components/Footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -28,7 +16,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      
       <body className="bg-gray-100 min-h-screen">
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.1/css/all.min.css"></link>
         <Navbar />
