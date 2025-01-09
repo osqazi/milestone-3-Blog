f50f58814d9cb75b00803c402933a6627e682388
osqazi <osqazi@gmail.com>
2025-01-09 15:08:48 +0500

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react"
 
 
 export const metadata: Metadata = {
@@ -21,6 +22,7 @@ export default function RootLayout({
         <Navbar />
         <div className="pt-32">
         {children}
+        <Analytics />
         <Footer/>
         </div>
       </body>
